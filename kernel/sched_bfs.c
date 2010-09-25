bfe095b33937a7a391bdba48f8bd650980202ce3
kingklick <erikbrokop@gmail.com>
2010-09-25 13:57:39 -0700

    Fixes to BFS scheduler test patch to compile
    These particular lines of code are not ready yet and were stopping a successful compile. Will add back in when ready.

--- kernel/sched_bfs.c
+++ kernel/sched_bfs.c
@@ -6822,34 +6822,7 @@ cputime_t task_stime(struct task_struct *p)
 
 /*
  * Must be called with siglock held.
- */
-void thread_group_times(struct task_struct *p, cputime_t *ut, cputime_t *st)
-{
-	struct signal_struct *sig = p->signal;
-	struct task_cputime cputime;
-	cputime_t rtime, utime, total;
-
-	thread_group_cputime(p, &cputime);
-
-	total = cputime_add(cputime.utime, cputime.stime);
-	rtime = nsecs_to_cputime(cputime.sum_exec_runtime);
-
-	if (total) {
-		u64 temp;
-
-		temp = (u64)(rtime * cputime.utime);
-		do_div(temp, total);
-		utime = (cputime_t)temp;
-	} else
-		utime = rtime;
-
-	sig->prev_utime = max(sig->prev_utime, utime);
-	sig->prev_stime = max(sig->prev_stime,
-			      cputime_sub(rtime, sig->prev_utime));
-
-	*ut = sig->prev_utime;
-	*st = sig->prev_stime;
-}
+ */// Removed this code for now it will not compile with it included
 #endif
 
 inline cputime_t task_gtime(struct task_struct *p)
